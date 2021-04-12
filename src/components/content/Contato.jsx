9198c823e1ca44d1efca852b3276812b6502570b
raulpereira1 <raul.pereira1@outlook.com>
2021-04-12 20:06:48 -0300

    Primeiro Comit

---
+++ src/components/content/Contato.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Contato.css'
+
+const Contato = () => {
+    return (
+        <div className="Contato">
+
+            <div className="Card">
+
+                <h1>Contato</h1>
+                <ul>
+
+
+                    <li>Instagram <button><a target="_blank" href='https://instagram.com/meudeusraul?igshid=1qhgc309p6vuz'> INSTAGRAM</a> </button></li>
+                    <li>Facebook  <button><a target="_blank" href='https://www.facebook.com/bspgeradores/'> FACEBOOK</a> </button> </li>
+                    
+                    <li>Rua Padre Pedro Guerra, 433 - Rio Pequeno, São José dos Pinhais - PR </li>
+                </ul>
+
+            </div>
+            <div className="background">
+            </div>
+
+        </div >
+    )
+
+}
+export default Contato
